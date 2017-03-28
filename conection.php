2cd43e5590ab5ad6f1364279b3b027f15c0ae243
SHIN_NARITA <triplfjump1417@gmail.com>
2017-03-28 17:17:05 +0900

    2017-03-28_create new and connected DB

---
+++ conection.php
@@ -0,0 +1,12 @@
+<?php
+require_once('config.php');
+
+function connectPdo() {
+  try{
+    return new PDO(DSN, DB_USER, DB_PASSWORD);
+  } catch (PDOException $e) {
+    echo $e->getMessage();
+    exit;
+  }
+}
+?>
