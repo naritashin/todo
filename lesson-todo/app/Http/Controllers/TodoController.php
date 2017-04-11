81223a87110f7f70703d1c8e0a56e6b92446b83a
SHIN_NARITA <triplfjump1417@gmail.com>
2017-04-11 15:51:25 +0900

    2017-04-11_implement delete

--- lesson-todo/app/Http/Controllers/TodoController.php
+++ lesson-todo/app/Http/Controllers/TodoController.php
@@ -48,4 +48,12 @@ class TodoController extends Controller
 
     return redirect()->to('todo');
   }
+
+  public function destroy($id)
+  {
+    $data = $this->todo->find($id);
+    $data->delete();
+
+    return redirect()->to('todo');
+  }
 }
